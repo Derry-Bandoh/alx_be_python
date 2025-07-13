3bd1ee43712160f34cb91a063aff67dc92ad508a
abderry <derrybandoh100@gmail.com>
2025-07-13 10:29:00 +0000

    Basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,18 @@
+number1 = 10
+number2 = 5
+
+#Adding two numbers 
+sum = number1 + number2
+
+#Subtracting two numbers
+difference = number1 - number2
+
+#Multiplying two numbers
+product = number1 * number2
+
+#Printing results
+print("Addition of ", number1, "and ", number2, "is ", sum)
+
+print("Sbutraction of ", number1, "and ", number2, "is ", difference)
+
+print("Multiplication of ", number1, "and ",number2, "is ", product )
