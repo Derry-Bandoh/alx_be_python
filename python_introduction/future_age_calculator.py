0858b20cb4f56cce704a2eac1fceaa9d1a1add1d
abderry <derrybandoh100@gmail.com>
2025-07-13 11:06:14 +0000

    Calculating age of user in 2050

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+current_year = 27
+current_age = int(input("How old are you?"))
+new_age = current_year + current_age
+
+#Printing user's age in 2050 assunming we are in the year 2023
+print("In 2050, you will be ", new_age," years old.")
