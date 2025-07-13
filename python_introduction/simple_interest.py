f7d28d5bc637870e02a1e4238c7be44c931dc7d7
abderry <derrybandoh100@gmail.com>
2025-07-13 10:46:17 +0000

    Interest Calculations

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+principal = 1000
+rate = 0.05
+time = 3
+
+#Calculating interest 
+interest = principal * rate * time
+
+#Printing the results of interest calculations
+print("The simple interest is: ", interest) 
