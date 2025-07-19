b3cfefc13c3425c16f087a21b1e7f2142b46fe2c
abderry <derrybandoh100@gmail.com>
2025-07-19 10:41:00 +0000

    Changed resulits to result

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -6,19 +6,19 @@ operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case x if x == "+" :
         result = num1 + num2
-        print(f"The resulits is {result}")
+        print(f"The result is {result}")
     case x if x == "-" :
         result = num1 - num2
-        print(f"The resulits is {result}")
+        print(f"The result is {result}")
     case x if x == "*" :
         result = num1 * num2
-        print(f"The resulits is {result}")
+        print(f"The result is {result}")
     case x if x == "/" :
         if num2 == 0:
             print("Cannot divide by zero")
         else:
             result = num1 / num2
-            print(f"The resulits is {result}")
+            print(f"The result is {result}")
     case _ :
         print("Invalid!")
 
