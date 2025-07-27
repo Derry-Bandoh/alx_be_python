71f9cce98ab04f2839ea863f3972946cde954430
abderry <derrybandoh100@gmail.com>
2025-07-27 00:08:16 +0000

    changed data type declared in para,eter

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,22 +1,23 @@
-def perform_operation (num1: float,num2: float,operation: str) :
+def perform_operation (num1,num2,operation) :
     match operation:
         case x if x == "add":
             results = num1 + num2
-            print(results)
+            
         case x  if x == "subtract":
             results = num1 - num2
-            print(results)
+            
         case x if x == "multiply":
             results = num1 * num2 
-            print(results)
+            
         case x if x == "divide":
             if num2 == 0:
-                print("Cannot divide by zero")
+                results = "Cannot divide by zero"
+                
             else:
                 results = num1 / num2 
-                print(results)
+                
         case _:
-            print("Invalid response")
+            results = "Invalid response"
     return results
 
             
