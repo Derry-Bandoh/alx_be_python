acecb7dfa5b583499c20077b0999c8a463903132
abderry <derrybandoh100@gmail.com>
2025-07-27 08:39:31 +0000

    Commented out match case and used if/else statement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -17,19 +17,33 @@ def perform_operation(num1, num2, operation):
     Returns:
         float or str: The result of the arithmetic operation, or an error message for division by zero
     """
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 == 0:
-                return "Cannot divide by zero"
-            return num1 / num2
-        case _:
-            return "Invalid operation"
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Cannot divide by zero"
+        else:
+            return num1/num2
+    else:
+        return "Invalid Operation"
+
+    # match operation:
+    #     case "add":
+    #         return num1 + num2
+    #     case "subtract":
+    #         return num1 - num2
+    #     case "multiply":
+    #         return num1 * num2
+    #     case "divide":
+    #         if num2 == 0:
+    #             return "Cannot divide by zero"
+    #         return num1 / num2
+    #     case _:
+    #         return "Invalid operation"
 
             
 
